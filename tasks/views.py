7fe13443290d5619f38054f2293e3f6045fa8709
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2025-01-29 20:55:25 +0600

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    # work with database connection
+    # Transform data
+    # data pass 
+    # HTTP response / json response
