ddc83666536776672275181f4aa3f2113060b542
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2025-02-11 19:46:09 +0600

    dashboard section

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def manager_dashboard(request):
-    return render(request, "admin-dashboard.html")
+    return render(request, "dashboard/manager-dashboard.html")
 
 def user_dashboard(request):
-    return render(request, "user-dashboard.html")
+    return render(request, "dashboard/user-dashboard.html")
